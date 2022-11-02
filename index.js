97fa5daeebaf56161487bb5925488f9233b9928a
Aktan3020 <116088191+Aktan3020@users.noreply.github.com>
2022-11-02 18:21:15 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+// задание1
+// let student ={
+//     id : 138124198,
+//     name:"Aktan",
+//     age:15,
+//     course:2,
+//     lesson:{
+//        lesson1:{ name:"math",
+//         id:979,
+//         NameOfTeacher:"oleg",
+//     },
+//     lesson2:{ name:"Chemistri",
+//         id:979,
+//         NameOfTeacher:"Vitalia",
+//     }
+//     }
+// }
+// console.log(student)
+
+
+// задание 2
+// let mass = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+// let mass2 = [];
+
+// for (let i = 0; i <= mass.length; i++) {
+//     if (mass[i] > 10){
+//        mass2.push(mass[i])
+//     }
+// }
+// console.log(mass2)
+// задание3
+let country = prompt();
+switch (country){
+    case "Россия":
+        console.log("Москва")
+        break
+    case "Кыргызстан":
+            console.log("Бишкек")
+            break
+    case "Казакстан":
+        console.log("Нур-Султан")
+            break
+}
